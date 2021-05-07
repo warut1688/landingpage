88fae458c11c5e3340a00627509f489033d6ac30
warut <warut1688@gmail.com>
2021-05-07 19:30:56 +0700

    initail commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        sans: ["Montserrat",'Prompt','sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
